edd4ade5b4f2c7ae7832e1862991465f6ddfb87f
kevna <lakevna@gmail.com>
2021-04-06 23:22:12 +0100

    custom action for key config

--- vim_clutchify/__main__.py
+++ vim_clutchify/__main__.py
@@ -1,18 +1,42 @@
 #!/usr/bin/env python
 import sys
-from argparse import ArgumentParser, Namespace
+from collections import namedtuple
+from argparse import ArgumentParser, Namespace, Action
+from typing import Dict
 
 from evdev import ecodes
 
 from vim_clutchify.device import DeviceContext, DeviceError
 
 
-def parse_args(args = sys.argv) -> Namespace:
+CLUTCHIFY_BUILTINS: Dict[str, tuple] = {
+    'legacy': ('i', 'esc'),
+    'f-low': ('F11', 'F12'),
+    'f-high': ('F23', 'F24'),
+    'ptt': ('micmute',),
+}
+
+
+ClutchKeys = namedtuple('ClutchKeys', ['up', 'down'], defaults=CLUTCHIFY_BUILTINS['f-low'])
+
+
+class ClutchKeyAction(Action):
+    def __init__(self, *args, **kwargs):
+        super(ClutchKeyAction, self).__init__(*args, nargs='+', **kwargs)
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        assert 1 <= len(values) <=2
+        values = CLUTCHIFY_BUILTINS.get(values[0], values)
+        if len(values) == 1:
+            values = values * 2
+        setattr(namespace, self.dest, ClutchKeys(*values))
+
+
+def parse_args(args = sys.argv[1:]) -> Namespace:
     """Generate an ArgumentParser for commandline arguments."""
     parser = ArgumentParser(description='Convert down and up keystrokes to two separate taps.')
     parser.add_argument('--device', default='FootSwitch', dest='device_name')
-    parser.add_argument('--down', default='F11')
-    parser.add_argument('--up', default='F12')
+    parser.add_argument('--keys', action=ClutchKeyAction, default=ClutchKeys())
     return parser.parse_args(args)
 
 
@@ -24,14 +48,15 @@ def core_loop(config: Namespace) -> None:
         for event in device.event_loop():
             if event.type == ecodes.EV_KEY:  # pylint: disable=no-member
                 if event.value == 1:
-                    device.tap(config.down)
+                    device.tap(config.keys.down)
                 elif event.value == 0:
-                    device.tap(config.up)
+                    device.tap(config.keys.up)
 
 
 def main() -> None:
     """CLI entrypoint."""
     config = parse_args()
+    print(config)
     try:
         core_loop(config)
     except DeviceError as error:
